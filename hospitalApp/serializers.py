72954fcc7755f7dbfc57b6b9e014b85febb98d4a
snehalnp <112758811+snehalnp@users.noreply.github.com>
2024-05-01 13:04:50 +0530

    Update serializers.py

--- hospitalApp/serializers.py
+++ hospitalApp/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
-from .models import User, Patient, Report
+from .models import User, Doctor, Patient, Report
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
